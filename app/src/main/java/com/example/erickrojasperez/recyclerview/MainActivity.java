a4088c18cb5d6dee1e9dd3fd786ad73fee6eace8
kalelman <erick_rojas_perez@hotmail.com>
2018-03-15 22:52:57 -0600

    Implementacion de mejora con la funcionalidad de agregar y eliminar elementos o vistas del RecyclerView

--- app/src/main/java/com/example/erickrojasperez/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/erickrojasperez/recyclerview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.erickrojasperez.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +37,37 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new RecyclerAdapter(names, R.layout.recycler_view_item, new RecyclerAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String name, int position) {
-                Toast.makeText(MainActivity.this, name + " - " + position, Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, name + " - " + position, Toast.LENGTH_LONG).show();
+                deleteName(position);
             }
         });
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.add_name_id:
+                this.addName(0);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private List<String> getAllNames() {
@@ -59,5 +88,24 @@ public class MainActivity extends AppCompatActivity {
             add("Noemi");
 
         }};
+    }
+
+    /**
+     * Metodo que agrega un nuevo elmento a la lista con position
+     * @param position
+     */
+    private void addName(int position) {
+        names.add(position, "newName" + (++counter));
+        mAdapter.notifyItemInserted(position);
+        mLayoutManager.scrollToPosition(position);
+    }
+
+    /**
+     * Metodo que elimina un nombre de la lista mediante position
+     * @param position
+     */
+    private void deleteName(int position) {
+        names.remove(position);
+        mAdapter.notifyItemRemoved(position);
     }
 }
